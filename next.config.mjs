bf8dd01ac9df7ea70541d696a20f3da4d38cccbf
Mason <leemason@hawaii.edu>
2025-03-30 20:31:22 -1000

    Digits, Part 2

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        domains: [
+          'github.com',
+          'avatars0.githubusercontent.com',
+          'www.ics.hawaii.edu'
+        ],
+    },
+};
 
 export default nextConfig;
