0693bd263d409098d06b23bb5862fe8e2a6ccba3
Mason <leemason@hawaii.edu>
2025-04-01 20:18:01 -1000

    contacts table finished in 35 minutes

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,63 +1,37 @@
 import { getServerSession } from 'next-auth';
-import { Col, Container, Row, Table } from 'react-bootstrap';
-import StuffItemAdmin from '@/components/StuffItemAdmin';
+import { Col, Container, Row } from 'react-bootstrap';
+// import StuffItemAdmin from '@/components/StuffItemAdmin';
 import { prisma } from '@/lib/prisma';
 import { adminProtectedPage } from '@/lib/page-protection';
 import authOptions from '@/lib/authOptions';
+import { Contact } from '@/lib/validationSchemas';
+import ContactCard from '@/components/ContactCardAdmin';
 
+/** Render a list of stuff for the logged in user. */
 const AdminPage = async () => {
+  // Protect the page, only logged in users can access it.
   const session = await getServerSession(authOptions);
   adminProtectedPage(
     session as {
       user: { email: string; id: string; randomKey: string };
     } | null,
   );
-  const stuff = await prisma.stuff.findMany({});
-  const users = await prisma.user.findMany({});
+  const contacts: Contact[] = await prisma.contact.findMany({
+  });
 
   return (
     <main>
       <Container id="list" fluid className="py-3">
         <Row>
           <Col>
-            <h1>List Stuff Admin</h1>
-            <Table striped bordered hover>
-              <thead>
-                <tr>
-                  <th>Name</th>
-                  <th>Quantity</th>
-                  <th>Condition</th>
-                  <th>Owner</th>
-                  <th>Actions</th>
-                </tr>
-              </thead>
-              <tbody>
-                {stuff.map((item) => (
-                  <StuffItemAdmin key={item.id} {...item} />
-                ))}
-              </tbody>
-            </Table>
-          </Col>
-        </Row>
-        <Row>
-          <Col>
-            <h1>List Users Admin</h1>
-            <Table striped bordered hover>
-              <thead>
-                <tr>
-                  <th>Email</th>
-                  <th>Role</th>
-                </tr>
-              </thead>
-              <tbody>
-                {users.map((user) => (
-                  <tr key={user.id}>
-                    <td>{user.email}</td>
-                    <td>{user.role}</td>
-                  </tr>
-                ))}
-              </tbody>
-            </Table>
+            <h2 className="text-center">List Contacts (Admin)</h2>
+            <Row xs={1} md={2} lg={3} className="g-4">
+              {contacts.map((contact) => (
+                <Col key={`Contact-${contact.firstName}`}>
+                  <ContactCard contact={contact} />
+                </Col>
+              ))}
+            </Row>
           </Col>
         </Row>
       </Container>
